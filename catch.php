07fc98c434963160bef2cdd4ea2d33b4f6b0fab7
Ubuntu <ubuntu@ip-172-31-46-109.ap-south-1.compute.internal>
2022-06-26 19:36:29 +0000

    Push main

---
+++ catch.php
@@ -0,0 +1,22 @@
+<?php 
+//$feedback_txt = $_POST['suggestion'];
+$conn = mysqli_connect("localhost", "mumbai","Mellob1989@", "temp"); 
+$feedback_txt = $conn->real_escape_string($_POST['suggestion']); 
+$query = "insert into feedback(feedback)values('".$feedback_txt."')"; 
+$result = mysqli_query($conn, $query);
+if($result)
+//   die("gggg");
+//    echo '".$feedback_txt."'
+  echo '<script>alert("Message sent, you can always leave me a message here!")</script>'; 
+ // header("Location: http://message.cryptomine.network");
+else
+  die("Something terrible happened. Please try again. ");
+?>
+
+
+<!DOCTYPE html> <html> <head> <meta http-equiv="refresh" 
+    content="0; url='http://message.cryptomine.network'" />
+  </head> <body> <p>Please follow <a 
+    href="http://message.cryptomine.network">this link</a>.</p>
+  </body>
+</html>
---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){ 
+    Particles.init({ 
+    selector: '.background', 
+    maxParticles: 100, 
+    connectParticles: 'true', 
+    speed:40,
+    minDistance: 140, 
+    sizeVariations: 4, 
+    color: '#ffffff'
+  });
+}
