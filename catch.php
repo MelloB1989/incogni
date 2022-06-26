6fc24cf291a9fedd24b0089d4186b58fccd57334
root <root@ip-172-31-46-109.ap-south-1.compute.internal>
2022-06-26 20:03:53 +0000

    push

--- catch.php
+++ catch.php
@@ -1,10 +1,14 @@
-<?php 
+#Developed by MelloB (27th June, 2022).
+<?php
 //$feedback_txt = $_POST['suggestion'];
 $conn = mysqli_connect("localhost", "mumbai","Mellob1989@", "temp"); 
 //PUT YOUR MYSQL DB CREDENTIALS HERE, THESE CREDENTIALS ARE JUST EXAMPLE.
 $feedback_txt = $conn->real_escape_string($_POST['suggestion']); 
 $query = "insert into feedback(feedback)values('".$feedback_txt."')"; 
 $result = mysqli_query($conn, $query);
+$mgs = $feedback_txt;
+$url = 'https://wirepusher.com/send?id=bXFWmpsgE&title=Messenger&message='.$mgs.'&type=Incogni';
+$code = get_headers($url, 1);
 if($result)
 //   die("gggg");
 //    echo '".$feedback_txt."'
@@ -16,8 +20,8 @@ else
 
 
 <!DOCTYPE html> <html> <head> <meta http-equiv="refresh" 
-    content="0; url='http://message.cryptomine.network'" />
+    content="0; url='http://incogni.socify.co.in'" />
   </head> <body> <p>Please follow <a 
-    href="http://message.cryptomine.network">this link</a>.</p>
+    href="http://incogni.socify.co.in/incogni">this link</a>.</p>
   </body>
 </html>
